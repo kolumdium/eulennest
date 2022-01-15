e2ea48a08d16c73e755ef826ad68764f76f7d98c
Martin Plank <martin.plank@informatik.tu-freiberg.de>
2022-01-15 12:40:54 +0100

    remove nginx call from script

--- getpictures.sh
+++ getpictures.sh
@@ -5,4 +5,3 @@ wget -O images.zip https://www.dropbox.com/s/yuy8fx5c5wj8ths/images.zip?dl=1
 apk add --update p7zip
 7z x -p$INPUT images.zip -y
 chmod 755 /usr/share/nginx/html/images
-nginx -g "daemon off"
