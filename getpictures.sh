9277245c375b32d7dabf3d270d1970f9656b3efa
Martin Plank <martin.plank@informatik.tu-freiberg.de>
2022-01-15 12:31:24 +0100

    fixed typo

--- getpictures.sh
+++ getpictures.sh
@@ -3,6 +3,6 @@ cd /usr/share/nginx/html
 apk add --update wget
 wget -O images.zip https://www.dropbox.com/s/yuy8fx5c5wj8ths/images.zip?dl=1
 apk add --update p7zip
-7z x -p$INPUT images.zip
+7z x -p$INPUT images.zip -y
 chmod 755 /usr/share/nginx/html/images
-nginx -g demon off
+nginx -g "daemon off"
